8968c3139a8dbaf40ebfcddd665f21745dd98e09
yuliakhazieva <oppasaranhae@gmail.com>
2017-05-04 14:54:16 +0500

    Basketball

---
+++ Basketball/GameViewController.designer.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Basketball
+{
+	[Register("GameViewController")]
+	partial class GameViewController
+	{
+		void ReleaseDesignerOutlets()
+		{
+		}
+	}
+}
