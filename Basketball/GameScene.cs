bbfd42bf7bfa30051bc6c9e1de60c1e8d076e745
yuliakhazieva <oppasaranhae@gmail.com>
2017-05-08 01:34:54 +0500

    code connections

--- Basketball/GameScene.cs
+++ Basketball/GameScene.cs
@@ -7,6 +7,8 @@ using UIKit;
 
 namespace Basketball
 {
+	enum gameState { welcome, play, pause, tryAgain}
+
 	public class Shelf : SKSpriteNode
 	{
 		bool isLeft;
@@ -20,29 +22,52 @@ namespace Basketball
 	}
 	public class GameScene : SKScene
 	{
-		const int row = 6;
-		const int col = 4;
-
+		gameState thisGame;
 		protected GameScene(IntPtr handle) : base(handle){}
 
 		public override void DidMoveToView(SKView view)
 		{
-			Random rand = new Random();
-			Shelf[][] matrix = new Shelf[row][];
-			for (int i = 0; i < row; ++i) 
-			{
-				matrix[i] = new Shelf[col];
-				for (int j = 0; j < col; ++j)
-				{
-					int temp = rand.Next(2);
-					bool isLeft = false;
-					if (temp == 0)
-					{
-						isLeft = true;
-					}
-				//	matrix[i][j] = new Shelf(isLeft, );
-				}
-			}
+
+			var shelf00 = (SKSpriteNode)this.GetChildNode("shelf00");
+			var shelf01 = (SKSpriteNode)this.GetChildNode("shelf01");
+			var shelf02 = (SKSpriteNode)this.GetChildNode("shelf02");
+			var shelf03 = (SKSpriteNode)this.GetChildNode("shelf03");
+			var shelf10 = (SKSpriteNode)this.GetChildNode("shelf10");
+			var shelf11 = (SKSpriteNode)this.GetChildNode("shelf11");
+			var shelf12 = (SKSpriteNode)this.GetChildNode("shelf12");
+			var shelf13 = (SKSpriteNode)this.GetChildNode("shelf13");
+			var shelf20 = (SKSpriteNode)this.GetChildNode("shelf20");
+			var shelf21 = (SKSpriteNode)this.GetChildNode("shelf21");
+			var shelf22 = (SKSpriteNode)this.GetChildNode("shelf22");
+			var shelf23 = (SKSpriteNode)this.GetChildNode("shelf23");
+			var shelf30 = (SKSpriteNode)this.GetChildNode("shelf30");
+			var shelf31 = (SKSpriteNode)this.GetChildNode("shelf31");
+			var shelf32 = (SKSpriteNode)this.GetChildNode("shelf32");
+			var shelf33 = (SKSpriteNode)this.GetChildNode("shelf33");
+			var life1 = (SKSpriteNode)this.GetChildNode("life1");
+			var life2 = (SKSpriteNode)this.GetChildNode("life2");
+			var life3 = (SKSpriteNode)this.GetChildNode("life3");
+			var pauseButton = (SKSpriteNode)this.GetChildNode("pauseButton");
+
+			var levelLabel = (SKLabelNode)this.GetChildNode("levelLabel");
+
+			var pauseFog = (SKSpriteNode)this.GetChildNode("pauseFog");
+			var aboutLabel = (SKLabelNode)this.GetChildNode("aboutLabel");
+			var aboutLine = (SKSpriteNode)this.GetChildNode("aboutLine");
+			var aboutButton = (SKSpriteNode)this.GetChildNode("aboutButton");
+			var settingsLabel = (SKLabelNode)this.GetChildNode("settingsLabel");
+			var settingsLine = (SKSpriteNode)this.GetChildNode("settingsLine");
+			var settingsButton = (SKSpriteNode)this.GetChildNode("settingsButton");
+			var resumeLabel = (SKLabelNode)this.GetChildNode("resumeLabel");
+			var resumeLine = (SKSpriteNode)this.GetChildNode("resumeLine");
+			var resumeButton = (SKSpriteNode)this.GetChildNode("resumeButton");
+			var highScoreLabel = (SKLabelNode)this.GetChildNode("highScoreLabel");
+			var yourScoreLabel = (SKLabelNode)this.GetChildNode("yourScoreLabel");
+
+			var playLabel = (SKLabelNode)this.GetChildNode("playLabel");
+			var playButton = (SKSpriteNode)this.GetChildNode("playButton");
+
+
 		}
 
 		public override void TouchesBegan(NSSet touches, UIEvent evt)
@@ -50,10 +75,19 @@ namespace Basketball
 			foreach (var touch in touches)
 			{
 				var location = ((UITouch)touch).LocationInNode(this);
+
+
+			//	this.GetNodeAtPoint(location);
+
 			}
 		}
 
 		public override void Update(double currentTime)
+		{
+			
+		}
+
+		public enterPause() 
 		{
 			
 		}
