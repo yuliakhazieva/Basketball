51a10d0228166f56530bd86ddbafb8ab5fb80eb6
yuliakhazieva <oppasaranhae@gmail.com>
2017-05-14 18:25:35 +0300

    working on edges contact lgic

--- Basketball/GameScene.cs
+++ Basketball/GameScene.cs
@@ -28,14 +28,14 @@ namespace Basketball
 			this.Position = point;
 			this.Texture = FromImageNamed("shelf").Texture;
 			this.Size = FromImageNamed("shelf").Texture.Size;
-			this.ZRotation = isLeft ? -10 : 10; 
+			this.ZRotation = isLeft ? -10 : 10;
 		}
 
 	}
 
 	public class SKButtonNode : SKSpriteNode
 	{
-		
+
 		public delegate void buttDel();
 
 		public event buttDel buttPressed;
@@ -43,7 +43,7 @@ namespace Basketball
 
 		public override void TouchesBegan(NSSet touches, UIEvent evt)
 		{
-		   // buttTouched();
+			// buttTouched();
 		}
 		public override void TouchesEnded(NSSet touches, UIEvent evt)
 		{
@@ -51,7 +51,7 @@ namespace Basketball
 		}
 	}
 
-	public class GameScene : SKScene
+	public class GameScene : SKScene, ISKPhysicsContactDelegate
 	{
 		Timer timer1 = new Timer();
 		Timer timer2 = new Timer();
@@ -82,76 +82,130 @@ namespace Basketball
 		SKLabelNode nameLabel2;
 		SKLabelNode rememberLabel1;
 		SKLabelNode rememberLabel2;
+		SKSpriteNode ball;
+		int userChoice;
+		int rightChoice;
 
 		gameState thisGame;
 		protected GameScene(IntPtr handle) : base(handle) { }
 
-
 		public override void DidMoveToView(SKView view)
 		{
-			
+
 			timer1.Interval = 1000;
 			timer2.Interval = 10000;
 			this.PhysicsWorld.Gravity = new CGVector(0, -5);
 			Random rand = new Random();
+			//this.PhysicsWorld.ContactDelegate = this;
+			this.PhysicsWorld.DidBeginContact += (object sender, EventArgs e) => 
+			{
+				var contact = sender as SKPhysicsContact;
+				if (contact.BodyA.Node.Name == "ball")
+				{
+					SKSpriteNode ballContact = (SKSpriteNode)contact.BodyA.Node;
+					SKSpriteNode basketContact = (SKSpriteNode)contact.BodyB.Node;
 
-			timer1.Elapsed += (object sender, ElapsedEventArgs e) => 
+					switch (basketContact.Name)
+					{
+						case "basket1":
+						rightChoice = 1;
+						break;
+						case "basket2":
+						rightChoice = 2;
+						break;
+						case "basket3":
+						rightChoice = 3;
+						break;
+						case "basket4":
+						rightChoice = 4;
+						break;
+					}
+					
+				}
+				else 
+				{
+					SKSpriteNode ballContact = (SKSpriteNode)contact.BodyB.Node;
+					SKSpriteNode basketContact = (SKSpriteNode)contact.BodyA.Node;	
+
+					switch (basketContact.Name)
+					{
+						case "basket1":
+						rightChoice = 1;
+						break;
+						case "basket2":
+						rightChoice = 2;
+						break;
+						case "basket3":
+						rightChoice = 3;
+						break;
+						case "basket4":
+						rightChoice = 4;
+						break;
+					}
+				}
+
+				ball.RunAction(SKAction.Sequence(SKAction.ScaleTo((nfloat)2,0.5), SKAction.RotateByAngle(5, 1), SKAction.RemoveFromParent()));
+			};
+
+
+			timer1.Elapsed += (object sender, ElapsedEventArgs e) =>
 			{
 
-					rememberLabel1.RunAction(SKAction.FadeAlphaTo(0, 1));
-					rememberLabel2.RunAction(SKAction.FadeAlphaTo(0, 1));
-					Shelf[][] shelfArray = new Shelf[4][];
-					for (int i = 0; i < shelfArray.Length; ++i)
+				rememberLabel1.RunAction(SKAction.FadeAlphaTo(0, 1));
+				rememberLabel2.RunAction(SKAction.FadeAlphaTo(0, 1));
+				Shelf[][] shelfArray = new Shelf[4][];
+				for (int i = 0; i < shelfArray.Length; ++i)
+				{
+					shelfArray[i] = new Shelf[4];
+					for (int j = 0; j < shelfArray[i].Length; ++j)
 					{
-						shelfArray[i] = new Shelf[4];
-						for (int j = 0; j < shelfArray[i].Length; ++j)
-						{
-							CGPoint shelfPlace = new CGPoint(40 + 80 * j, 400 - 90 * i);
-							int temp = rand.Next(0, 2);
-							bool left = temp == 0 ? true : false;
-							if (j == 0) { left = true; } else if (j == 3) { left = false; }
-							shelfArray[i][j] = new Shelf(left, shelfPlace);
-							shelfArray[i][j].PhysicsBody = SKPhysicsBody.CreateRectangularBody(shelfArray[i][j].Size);
-							shelfArray[i][j].PhysicsBody.Dynamic = false;
-							shelfArray[i][j].PhysicsBody.Friction = (nfloat)0.1;
-							shelfArray[i][j].Name = "" + i + j;
-							this.AddChild(shelfArray[i][j]);
-						}
+						CGPoint shelfPlace = new CGPoint(40 + 80 * j, 400 - 90 * i);
+						int temp = rand.Next(0, 2);
+						bool left = temp == 0 ? true : false;
+						if (j == 0) { left = true; } else if (j == 3) { left = false; }
+						shelfArray[i][j] = new Shelf(left, shelfPlace);
+						shelfArray[i][j].PhysicsBody = SKPhysicsBody.CreateRectangularBody(shelfArray[i][j].Size);
+						shelfArray[i][j].PhysicsBody.Dynamic = false;
+						shelfArray[i][j].PhysicsBody.Friction = (nfloat)0.1;
+						shelfArray[i][j].Name = "" + i + j;
+						this.AddChild(shelfArray[i][j]);
+					}
 
 				}
 				timer1.Stop();
 				timer1.Close();
 				timer2.Start();
 			};
-			timer2.Elapsed += (object sender, ElapsedEventArgs e) => 
+			timer2.Elapsed += (object sender, ElapsedEventArgs e) =>
 			{
 				timer2.Stop();
 				timer2.Close();
-					for (int i = 0; i< 4; i++)
+				for (int i = 0; i < 4; i++)
+				{
+					for (int j = 0; j < 4; j++)
 					{
-						for (int j = 0; j< 4; j++) 
-						{
-                       		this.GetChildNode("" + i + j).RunAction(SKAction.FadeAlphaTo(0, 1));
-						}
+						this.GetChildNode("" + i + j).RunAction(SKAction.FadeAlphaTo(0, 1));
 					}
-					rememberLabel1.Text = "Куда попадет мяч?";
-					rememberLabel1.RunAction(SKAction.FadeAlphaTo(1, 1));
-					int ballCol = rand.Next(0, 4);
+				}
+				rememberLabel1.Text = "Куда попадет мяч?";
+				rememberLabel1.RunAction(SKAction.FadeAlphaTo(1, 1));
+				int ballCol = rand.Next(0, 4);
 
-					SKAction rotateBackAndForth = SKAction.Sequence(SKAction.RotateToAngle((nfloat)6.0, 0.5), SKAction.RotateToAngle((nfloat)6.5, 0.5));
-					basket1.RunAction(SKAction.RepeatActionForever(rotateBackAndForth));
-					basket2.RunAction(SKAction.RepeatActionForever(rotateBackAndForth));
-					basket3.RunAction(SKAction.RepeatActionForever(rotateBackAndForth));
-					basket4.RunAction(SKAction.RepeatActionForever(rotateBackAndForth));
+				SKAction rotateBackAndForth = SKAction.Sequence(SKAction.RotateToAngle((nfloat)6.0, 0.5), SKAction.RotateToAngle((nfloat)6.5, 0.5));
+				basket1.RunAction(SKAction.RepeatActionForever(rotateBackAndForth));
+				basket2.RunAction(SKAction.RepeatActionForever(rotateBackAndForth));
+				basket3.RunAction(SKAction.RepeatActionForever(rotateBackAndForth));
+				basket4.RunAction(SKAction.RepeatActionForever(rotateBackAndForth));
 
-					SKSpriteNode ball = SKSpriteNode.FromImageNamed("basketball");
-					ball.Position = new CGPoint(40+ballCol*80, 500);
-					ball.PhysicsBody = SKPhysicsBody.CreateCircularBody(5);
-					ball.PhysicsBody.Pinned = true;
-					ball.XScale = (nfloat)0.5;
-					ball.YScale = (nfloat)0.5;
-					ball.PhysicsBody.LinearDamping = 0;
-					AddChild(ball);
+
+				ball.Position = new CGPoint(40 + ballCol * 80, 500);
+				ball.PhysicsBody = SKPhysicsBody.CreateCircularBody(5);
+				ball.PhysicsBody.ContactTestBitMask = 1;
+				ball.PhysicsBody.Pinned = true;
+				ball.XScale = (nfloat)0.5;
+				ball.YScale = (nfloat)0.5;
+				ball.PhysicsBody.LinearDamping = 0;
+				AddChild(ball);
 
 			};
 			life1 = (SKSpriteNode)this.GetChildNode("life1");
@@ -161,35 +215,35 @@ namespace Basketball
 			pauseButton = new SKButtonNode();
 			pauseButton.Texture = SKSpriteNode.FromImageNamed("pause").Texture;
 			pauseButton.Size = SKSpriteNode.FromImageNamed("pause").Size;
-			pauseButton.Position = new CGPoint(42, 535);;
+			pauseButton.Position = new CGPoint(42, 535); ;
 			this.AddChild(pauseButton);
 
 			settingsButton = new SKButtonNode();
 			settingsButton.Texture = SKSpriteNode.FromImageNamed("pause").Texture;
 			settingsButton.Alpha = 0;
-			settingsButton.Position = new CGPoint(160,260);
-			settingsButton.Size = new CGSize(320,58);
+			settingsButton.Position = new CGPoint(160, 260);
+			settingsButton.Size = new CGSize(320, 58);
 			this.AddChild(settingsButton);
 
 			resumeButton = new SKButtonNode();
 			resumeButton.Texture = SKSpriteNode.FromImageNamed("pause").Texture;
 			resumeButton.Alpha = 0;
-			resumeButton.Position = new CGPoint(160,370);
-			resumeButton.Size = new CGSize(320,57);
-            this.AddChild(resumeButton);
+			resumeButton.Position = new CGPoint(160, 370);
+			resumeButton.Size = new CGSize(320, 57);
+			this.AddChild(resumeButton);
 
 			aboutButton = new SKButtonNode();
 			aboutButton.Texture = SKSpriteNode.FromImageNamed("pause").Texture;
 			aboutButton.Alpha = 0;
-			aboutButton.Position = new CGPoint(160,20);
-			aboutButton.Size = new CGSize(323,46);
+			aboutButton.Position = new CGPoint(160, 20);
+			aboutButton.Size = new CGSize(323, 46);
 			this.AddChild(aboutButton);
 
 			playButton = new SKButtonNode();
 			playButton.Texture = SKSpriteNode.FromImageNamed("pause").Texture;
 			playButton.Alpha = 0;
-			playButton.Position = new CGPoint(160,203);
-			playButton.Size = new CGSize(321,57);
+			playButton.Position = new CGPoint(160, 203);
+			playButton.Size = new CGSize(321, 57);
 			this.AddChild(playButton);
 			playButton.ZPosition = 20;
 
@@ -197,7 +251,7 @@ namespace Basketball
 			basket2 = makeABasket(basket1, 120);
 			basket3 = makeABasket(basket1, 200);
 			basket4 = makeABasket(basket1, 280);
-		
+
 			levelLabel = (SKLabelNode)this.GetChildNode("levelLabel");
 
 			pauseFog = (SKSpriteNode)this.GetChildNode("pauseFog");
@@ -214,11 +268,31 @@ namespace Basketball
 			nameLabel2 = (SKLabelNode)this.GetChildNode("nameLabel2");
 			rememberLabel1 = (SKLabelNode)this.GetChildNode("rememberLabel1");
 			rememberLabel2 = (SKLabelNode)this.GetChildNode("rememberLabel2");
+			ball = SKSpriteNode.FromImageNamed("basketball");
+			ball.Name = "ball";
+			basket1.PhysicsBody = SKPhysicsBody.CreateRectangularBody(new CGSize(48,48), basket1.Position);
+			basket2.PhysicsBody = SKPhysicsBody.CreateRectangularBody(new CGSize(48,48), basket2.Position);
+			basket3.PhysicsBody = SKPhysicsBody.CreateRectangularBody(new CGSize(48,48), basket3.Position);
+			basket4.PhysicsBody = SKPhysicsBody.CreateRectangularBody(new CGSize(48,48), basket4.Position);
+
+			basket1.PhysicsBody.Dynamic = false;
+			basket2.PhysicsBody.Dynamic = false;
+			basket3.PhysicsBody.Dynamic = false;
+			basket4.PhysicsBody.Dynamic = false;
+
+			basket1.PhysicsBody.CollisionBitMask = 0;
+			basket2.PhysicsBody.CollisionBitMask = 0;
+			basket3.PhysicsBody.CollisionBitMask = 0;
+			basket4.PhysicsBody.CollisionBitMask = 0;
 
 			pauseButton.buttPressed += pausePressed;
 			playButton.buttPressed += playPressed;
 			settingsButton.buttPressed += settingsPressed;
 			aboutButton.buttPressed += aboutPressed;
+			basket1.buttPressed += basket1Pressed;
+			basket2.buttPressed += basket2Pressed;
+			basket3.buttPressed += basket3Pressed;
+			basket4.buttPressed += basket4Pressed;
 
 			pauseButton.Hidden = true;
 			settingsLine.Hidden = true;
@@ -241,6 +315,17 @@ namespace Basketball
 			resumeButton.UserInteractionEnabled = true;
 			aboutButton.UserInteractionEnabled = true;
 			settingsButton.UserInteractionEnabled = true;
+			basket1.UserInteractionEnabled = true;
+			basket2.UserInteractionEnabled = true;
+			basket3.UserInteractionEnabled = true;
+			basket4.UserInteractionEnabled = true;
+
+			basket1.PhysicsBody.ContactTestBitMask = 1;
+			basket2.PhysicsBody.ContactTestBitMask = 1;
+			basket3.PhysicsBody.ContactTestBitMask = 1;
+			basket4.PhysicsBody.ContactTestBitMask = 1;
+
+
 
 			playButton.Alpha = (float)0.000001;
 		}
@@ -261,22 +346,13 @@ namespace Basketball
 		{
 			foreach (var touch in touches)
 			{
-				//var location = ((UITouch)touch).LocationInNode(this);
-				//SKNode touchedNode = this.GetNodeAtPoint(location);
-				//SKSpriteNode ball = SKSpriteNode.FromImageNamed("basketball");
-				//ball.Position = location;
-				//ball.PhysicsBody = SKPhysicsBody.CreateCircularBody(5);
-				//ball.PhysicsBody.Pinned = false;
-				//ball.XScale = (nfloat)0.5;
-				//ball.YScale = (nfloat)0.5;
-				//ball.PhysicsBody.LinearDamping = 0;
-				//AddChild(ball);
+
 			}
 		}
 
 		public override void Update(double currentTime)
 		{
-			
+
 		}
 
 		public void pausePressed()
@@ -312,18 +388,62 @@ namespace Basketball
 
 		public void playPressed()
 		{
+			playButton.RemoveFromParent();
 			pauseFog.Hidden = true;
 			nameLabel1.Hidden = true;
 			nameLabel2.Hidden = true;
 			playLabel.Hidden = true;
 			rememberLabel1.Hidden = false;
 
-			rememberLabel1.RunAction(SKAction.FadeAlphaTo(1,1));
+			rememberLabel1.RunAction(SKAction.FadeAlphaTo(1, 1));
 			rememberLabel2.Hidden = false;
-			rememberLabel2.RunAction(SKAction.FadeAlphaTo(1,1));
+			rememberLabel2.RunAction(SKAction.FadeAlphaTo(1, 1));
 
 			timer1.Start();
 
 		}
+
+		public void basket1Pressed()
+		{
+			userChoice = 1;
+			fall();
+		}
+
+		public void basket2Pressed()
+		{
+			userChoice = 2;
+			fall();
+		}
+
+		public void basket3Pressed()
+		{
+			userChoice = 3;
+			fall();
+		}
+
+		public void basket4Pressed()
+		{
+			userChoice = 4;
+			fall();
+		}
+
+		public void fall()
+		{
+			basket1.RemoveAllActions();
+			basket2.RemoveAllActions();
+			basket3.RemoveAllActions();
+			basket4.RemoveAllActions();
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					this.GetChildNode("" + i + j).RunAction(SKAction.FadeAlphaTo(1, 1));
+				}
+			}
+			ball.PhysicsBody.Pinned = false;
+
+		}
+
+
 	}
 }
